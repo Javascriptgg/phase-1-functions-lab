d16edeace725fb93939fb0d9b63c484cd9959c90
David Muchahi <Muchahi853@gmail.com>
2024-10-17 15:32:33 +0300

    Finished lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(Blocks){
+    if(Blocks>42){
+        return Blocks-42;
+    }else if(Blocks<42){
+        return 42-Blocks;
+    }else{
+        return 0;
+    }
+
+}
+function distanceFromHqInFeet(Blocks){
+    return distanceFromHqInBlocks(Blocks)*264;
+}
+function distanceTravelledInFeet(start,end){
+    return Math.abs(start*264-end*264);
+}
+function calculatesFarePrice(start,destination){
+    const totalDistanceTravelled=Math.abs(start*264-destination*264);
+    if(totalDistanceTravelled<400){
+        return 0;
+    }else if(totalDistanceTravelled<2000){
+        return 2.56;
+    }else if(totalDistanceTravelled<2500){
+        return 25;
+    }else{
+        return "cannot travel that far";
+    }
+}
